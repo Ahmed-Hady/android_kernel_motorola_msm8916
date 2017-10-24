bdf26ac9853026a5c5da1bfacbddfe3c4e922936
Haibin Liu <haibinl@qti.qualcomm.com>
2017-10-24 20:32:48 +0800

    msm: sensor: actuator: avoid accessing out of bound memory
    Issue:
    When total_steps is updated, after that, copy_from_user
    fails with an error, then, i2c_reg_tbl is not allocated.
    In this case, when calling msm_actuator_parse_i2c_params,
    it lead to out-of-bound memory write.
    
    Fix:
    1) Assign total_steps to zero when error from copying.
    2) Add NULL pointer check for i2c tbl.
    
    CRs-Fixed: 2111672
    Change-Id: Ib9dcb182356e2df8078c131edfd0791fa95a35e0
    Signed-off-by: Haibin Liu <haibinl@codeaurora.org>
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -105,6 +105,9 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 		(!a_ctrl->reg_tbl) ||
 		(!a_ctrl->i2c_reg_tbl)) {
 		pr_err("failed. NULL actuator pointers");
+
+	if (a_ctrl->i2c_reg_tbl == NULL) {
+		pr_err("failed. i2c reg tabl is NULL");
 		return;
 	}
 
@@ -1262,6 +1265,7 @@ static int32_t msm_actuator_set_param(struct msm_actuator_ctrl_t *a_ctrl,
 	if (copy_from_user(&a_ctrl->region_params,
 		(void *)set_info->af_tuning_params.region_params,
 		a_ctrl->region_size * sizeof(struct region_params_t))) {
+		a_ctrl->total_steps = 0;
 		pr_err("Error copying region_params\n");
 		return -EFAULT;
 	}
