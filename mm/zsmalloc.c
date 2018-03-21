af78d7d214c578b68e7711dc30ae516d56f530d2
Ahmed Hady <ahmedhady6@gmail.com>
2018-03-21 03:11:36 +0200

    Revert "zsmalloc: use class->pages_per_zspage"
    This reverts commit 0db1f4d1586e9f534f7841705e370961aef4d193.

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1709,7 +1709,7 @@ static unsigned long zs_can_compact(struct size_class *class)
 	obj_wasted /= get_maxobj_per_zspage(class->size,
 			class->pages_per_zspage);
 
-	return obj_wasted * class->pages_per_zspage;
+	return obj_wasted * get_pages_per_zspage(class->size);
 }
 
 static unsigned long __zs_compact(struct zs_pool *pool,
@@ -1749,8 +1749,7 @@ static unsigned long __zs_compact(struct zs_pool *pool,
 			break;
 
 		putback_zspage(pool, class, dst_page);
-		if (putback_zspage(pool, class, src_page) == ZS_EMPTY)
-			pool->stats.pages_compacted += class->pages_per_zspage;
+		putback_zspage(pool, class, src_page);
 		spin_unlock(&class->lock);
 		nr_total_migrated += cc.nr_migrated;
 		cond_resched();
